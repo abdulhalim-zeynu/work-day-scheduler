4b67fadc37c71aa3ef5e7c60a15cfa42a55cdff1
Abdulhalim Zeynu <a.zeynu8@gmail.com>
2020-08-03 00:16:09 -0500

    made progress on HW

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,2 @@
-//Displays current date and time
-var now = dateFormat(new Date(), "dddd, mmmm dS, yyyy, h:MM:ss TT");
-    
-$('#currentDay').append(now);
+moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(moment());
