f864458d40ba355c008786521767f230fea26680
Abdulhalim Zeynu <a.zeynu8@gmail.com>
2020-08-03 22:40:47 -0500

    updated HW assignment

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,9 @@
-var now = moment();
-console.log(now);
+// current date and time
+$(document).ready(function () {
+    var now = moment();
+    console.log(now);
+    console.log($("#currentDay"));
+    $("#currentDay").text(now);
+});
+
+
